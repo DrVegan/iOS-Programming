141984d327f82749fdf51e92f5e3aacd6f220e42
Daniel Freeman <dr.freeman@me.com>
2016-09-29 19:59:14 +1000

    Finished Prototype of beacons in range

--- Hacking_with_Swift/Project22/Project22/ViewController.swift
+++ Hacking_with_Swift/Project22/Project22/ViewController.swift
@@ -11,11 +11,16 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
-    var locationManager: CLLocationManager!
-
     
-    @IBOutlet weak var distanceReading: UILabel!
-    @IBOutlet weak var secondBeacon: UILabel!
+
+    @IBOutlet weak var label0: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    
+    var locationManager: CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +30,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestAlwaysAuthorization()
         
         view.backgroundColor = UIColor.gray
-        self.distanceReading.text = "WELCOME"
+        self.label0.text = "label 0"
+        self.label1.text = "label 1"
+        self.label2.text = "label 2"
+        self.label3.text = "label 3"
+        self.label4.text = "label 4"
+        self.label5.text = "label 5"
         
         
     }
@@ -43,17 +53,38 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-        print("Nnumber of Beacons: \(beacons.count)")
-        if beacons.count == 1{
-    
-            update(distance: beacons[0].proximity, label: distanceReading)
-        }
-        if beacons.count == 2{
-                    self.secondBeacon.text = "Beacon 2"
-            update(distance: beacons[1].proximity, label: secondBeacon)
+        
+        let numberOfBeacons = beacons.count
+        print("Number of Beacons: \(numberOfBeacons)")
+        
+        
+        
+        if numberOfBeacons > 0{
+            for num in 0...numberOfBeacons-1{
+                switch num {
+                case 0:
+                    update(beacon: beacons[num], label: label0)
+                case 1:
+                    update(beacon: beacons[num], label: label1)
+                case 2:
+                    update(beacon: beacons[num], label: label2)
+                case 3:
+                    update(beacon: beacons[num], label: label3)
+                case 4:
+                    update(beacon: beacons[num], label: label4)
+                case 5:
+                    update(beacon: beacons[num], label: label5)
+                    
+                default:
+                        print("Default SwitchCase")
+                    
+                }
+            }
+        }else{
+            print("NO BEACONS FOUND, Count is 0")
         }
     }
- 
+      
     func startScanning() {
         let uuid = UUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!
         //let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 61014, minor: 8459, identifier: "MyBeacon")
@@ -63,24 +94,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startRangingBeacons(in: beaconRegion)
     }
     
-    func update(distance: CLProximity, label: UILabel) {
+    func update(beacon: CLBeacon, label: UILabel) {
         UIView.animate(withDuration: 0.8) { [unowned self] in
-            switch distance {
+            self.view.backgroundColor = UIColor.gray
+            
+            switch beacon.proximity {
             case .unknown:
-                self.view.backgroundColor = UIColor.gray
-                label.text = "UNKNOWN"
+                label.backgroundColor = UIColor.gray
+                label.text = "Maj:\(beacon.major) ProxRaw:\(beacon.proximity.rawValue) ProxHash:\(beacon.proximity.hashValue) Accu:\(beacon.accuracy)"
+                
                 
             case .far:
-                self.view.backgroundColor = UIColor.blue
-                label.text = "FAR"
+                label.backgroundColor = UIColor.blue
+                label.text = "Maj:\(beacon.major) ProxRaw:\(beacon.proximity.rawValue) ProxHash:\(beacon.proximity.hashValue) Accu:\(beacon.accuracy)"
                 
             case .near:
-                self.view.backgroundColor = UIColor.orange
-                label.text = "NEAR"
+                label.backgroundColor = UIColor.orange
+                label.text = "Maj:\(beacon.major) ProxRaw:\(beacon.proximity.rawValue) ProxHash:\(beacon.proximity.hashValue) Accu:\(beacon.accuracy)"
                 
             case .immediate:
-                self.view.backgroundColor = UIColor.red
-                label.text = "RIGHT HERE"
+                label.backgroundColor = UIColor.red
+                label.text = "Maj:\(beacon.major) ProxRaw:\(beacon.proximity.rawValue) ProxHash:\(beacon.proximity.hashValue) Accu:\(beacon.accuracy)"
                 
             }
         }
